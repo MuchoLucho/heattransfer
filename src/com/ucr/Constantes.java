ca248fb8efc5ce220929f03335bc9b388c6cf6ec
Luis Ramirez <lerv22@gmail.com>
2019-06-08 19:00:59 -0600

    Segundo 0

--- src/com/ucr/Constantes.java
+++ src/com/ucr/Constantes.java
@@ -1,31 +1,28 @@
 package com.ucr;
 
 public class Constantes {
-    private  static double DIFU_TERM_COB = 0.000117;
-    private  static double DIFU_TERM_ACER = 0.0000188;
-    private  static double LARGO_COBRE = 0.05;
-    private  static double LARGO_ACERO = 0.05;
-    private  static double ANCHO_COB = 0.05;
-    private  static double ANCHO_ACE = 0.05;
-    private  static double DIST_NODAL = 0.0005;
-    private  static int TEMP_INF = 300;
-    private  static int TEMP_COB = 600;
-    private  static int TEMP_ACER_IZQ = 1100;
-    private  static int TEMP_ACER_CENT = 950;
-    private  static int TEMP_ACER_DER = 800;
-    private  static int COEF_CONV = 13;
-    private  static int RESIS_SOLDA = 200000;
-    private  static int COND_TERM_COBR = 401;
-    private  static double COND_TERM_ACER = 60.5;
-    private  static int TIEMPO = 20;
+    private static double DIFU_TERM_COB = 0.000117;
+    private static double DIFU_TERM_ACER = 0.0000188;
+    private static double LADO_PLACA = 0.05;
+    private static double DIST_NODAL = 0.0005;
+    private static int TEMP_INF = 300;
+    private static int TEMP_COB = 600;
+    private static int TEMP_ACER_IZQ = 1100;
+    private static int TEMP_ACER_CENT = 950;
+    private static int TEMP_ACER_DER = 800;
+    private static int COEF_CONV = 13;
+    private static int RESIS_SOLDA = 200000;
+    private static int COND_TERM_COBR = 401;
+    private static double COND_TERM_ACER = 60.5;
+    private static int TIEMPO = 20;
 
 
     public static int getCANT_NODOS_X(){
-        return (int) ((int)(LARGO_ACERO+LARGO_COBRE)/DIST_NODAL);
+        return (int) ((2* LADO_PLACA)/DIST_NODAL);
     }
 
     public static int getCANT_NODOS_Y(){
-        return (int) ((int)(ANCHO_ACE)/DIST_NODAL);
+        return (int) ((LADO_PLACA)/DIST_NODAL);
     }
 
     public static int getCOND_TERM_COBR() {
@@ -53,36 +50,12 @@ public class Constantes {
         Constantes.DIFU_TERM_ACER = DIFU_TERM_ACER;
     }
 
-    public static double getLARGO_COBRE() {
-        return LARGO_COBRE;
-    }
-
-    public static void setLARGO_COBRE(double LARGO_COBRE) {
-        Constantes.LARGO_COBRE = LARGO_COBRE;
-    }
-
-    public static double getLARGO_ACERO() {
-        return LARGO_ACERO;
-    }
-
-    public static void setLARGO_ACERO(double LARGO_ACERO) {
-        Constantes.LARGO_ACERO = LARGO_ACERO;
-    }
-
     public static double getANCHO_COB() {
-        return ANCHO_COB;
+        return LADO_PLACA;
     }
 
     public static void setANCHO_COB(double ANCHO_COB) {
-        Constantes.ANCHO_COB = ANCHO_COB;
-    }
-
-    public static double getANCHO_ACE() {
-        return ANCHO_ACE;
-    }
-
-    public static void setANCHO_ACE(double ANCHO_ACE) {
-        Constantes.ANCHO_ACE = ANCHO_ACE;
+        Constantes.LADO_PLACA = ANCHO_COB;
     }
 
     public static double getDIST_NODAL() {
--- src/com/ucr/Principal.java
+++ src/com/ucr/Principal.java
@@ -1,10 +1,39 @@
 package com.ucr;
 
-public class Main {
+public class Principal {
+    private double matriz [][];
+
+    public Principal() {
+        this.matriz = new double [Constantes.getCANT_NODOS_X()][Constantes.getCANT_NODOS_Y()];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if(j < matriz[i].length/2){ // Es cobre
+                    matriz[i][j] =  Constantes.getTEMP_COB();
+                }else { // Es acero
+                    if (j == (matriz[i].length/2)) // Es primera linea de acero?
+                        matriz[i][j] =  Constantes.getTEMP_ACER_IZQ();
+                    else if (j == matriz[i].length-1) // Es última linea de acero?
+                        matriz[i][j] =  Constantes.getTEMP_ACER_DER();
+                    else // acero del centro
+                        matriz[i][j] =  Constantes.getTEMP_ACER_CENT();
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
-	// write your code here
-        double matriz [][] = new double [Constantes.getCANT_NODOS_X()][Constantes.getCANT_NODOS_Y()];
+        Principal principal = new Principal();
+        principal.printer();
+
+    }
+
+    public void printer(){
+        for (int i = 0; i < this.matriz.length; i++) {
+            for (int j = 0; j < this.matriz[i].length; j++) {
+                System.out.print(this.matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static int getT_ESQ_SUP_IZQ_COB(){
